f49846edbd90224f015a4fd14f0748f78374d040
Geckoyamori <69742111+Geckoyamori@users.noreply.github.com>
2021-04-18 23:12:27 +0900

    筋トレメニュータップ時の画面遷移を作成

--- MuscleRecommend/MuscleRecommend/Scene/Main/ViewController.swift
+++ MuscleRecommend/MuscleRecommend/Scene/Main/ViewController.swift
@@ -7,11 +7,13 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+class ViewController: UIViewController {
     // navigationbarのタイトル
     let navigationBarTitle = "筋トレメニュー"
     // 筋トレメニューを格納するリスト
     var muscleMenuList = [String]()
+    // 選択された筋トレメニュー
+    var selectedMuscleMenu: String?
     // 筋トレメニューを表示するtableView
     @IBOutlet weak var muscleMenuTableView: UITableView!
     
@@ -21,12 +23,64 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // navigationbarの設定
         title = navigationBarTitle
         
+        muscleMenuTableView.delegate = self
+        muscleMenuTableView.dataSource = self
+        
         // 筋トレメニューを格納するリストをUserDefaultsから取得
         if UserDefaults.standard.object(forKey: "muscleMenuList") != nil {
             muscleMenuList = UserDefaults.standard.object(forKey: "muscleMenuList") as! [String]
         }
     }
     
+    // segueの準備
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toMuscleRecordHistoryViewController") {
+            let muscleRecordHistoryViewController: MuscleRecordHistoryViewController = segue.destination as! MuscleRecordHistoryViewController
+            muscleRecordHistoryViewController.navigationBarTitle = selectedMuscleMenu!
+        }
+    }
+    
+    // navigationbarの追加ボタンをタップした場合の処理
+    @IBAction func tapAddButton(_ sender: Any) {
+        // 筋トレメニュー追加のアラートダイアログを表示
+        showAddMuscleMenuDialog()
+    }
+    
+    // 筋トレメニュー追加のアラートダイアログを表示
+    func showAddMuscleMenuDialog() {
+        // アラートダイアログを定義
+        let alert = UIAlertController(
+            title: "筋トレメニューの追加",
+            message: "追加する筋トレメニューを入力してください。",
+            preferredStyle: UIAlertController.Style.alert)
+        // Cancel, OKボタンを定義
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        let okAction = UIAlertAction(title: "OK", style: .default) { [self] (_) in
+            if let textField = alert.textFields?.first {
+                muscleMenuList.append(textField.text!)
+                UserDefaults.standard.set(muscleMenuList, forKey: "muscleMenuList")
+            }
+            muscleMenuTableView.reloadData()
+        }
+        alert.addAction(cancelAction)
+        alert.addAction(okAction)
+        okAction.isEnabled = false
+        // アラートダイアログにテキストフィールドを定義
+        alert.addTextField{(textField) in
+            // textFieldが空白の時はOKボタンを非活性化
+            NotificationCenter.default.addObserver(forName: UITextField.textDidChangeNotification, object: textField, queue: OperationQueue.main, using: {_ in
+                let textCount = textField.text!.trimmingCharacters(in: .whitespacesAndNewlines).count
+                okAction.isEnabled = textCount > 0
+            })
+        }
+        // アラートダイアログを表示
+        self.present(alert, animated: true, completion: nil)
+    }
+
+}
+
+// tableViewのメソッド
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
     // tableViewの行数を設定
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return muscleMenuList.count
@@ -41,38 +95,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == UITableViewCell.EditingStyle.delete {
             muscleMenuList.remove(at: indexPath.row)
+            UserDefaults.standard.set(muscleMenuList, forKey: "muscleMenuList")
             muscleMenuTableView.deleteRows(at: [indexPath as IndexPath], with: UITableView.RowAnimation.automatic)
         }
+    }    
+    // テーブルビューのセル選択時の画面遷移
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        selectedMuscleMenu = muscleMenuList[indexPath.row]
+        performSegue(withIdentifier: "toMuscleRecordHistoryViewController", sender: nil)
     }
-    
-    // navigationbarの追加ボタンをタップした場合の処理
-    @IBAction func tapAddButton(_ sender: Any) {
-        // アラートダイアログを定義
-        let alert = UIAlertController(
-            title: "筋トレメニューの追加",
-            message: "追加する筋トレメニューを入力してください。",
-            preferredStyle: UIAlertController.Style.alert)
-        // アラートダイアログにテキストフィールドを定義
-        alert.addTextField(configurationHandler: nil)
-        alert.addAction(
-            UIAlertAction(
-                title: "Cancel",
-                style: UIAlertAction.Style.cancel,
-                handler: nil))
-        alert.addAction(
-            UIAlertAction(
-                title: "OK",
-                style: UIAlertAction.Style.default) { [self] _ in
-                if let textField = alert.textFields?.first {
-                    muscleMenuList.append(textField.text!)
-                    UserDefaults.standard.set(muscleMenuList, forKey: "muscleMenuList")
-                }
-                muscleMenuTableView.reloadData()
-            }
-        )
-        // アラートダイアログを表示
-        self.present(alert, animated: true, completion: nil)
-    }
-
 }
-
---
+++ MuscleRecommend/MuscleRecommend/Scene/MuscleRecordHistory/MuscleRecordHistoryViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MuscleRecordHistoryViewController.swift
+//  MuscleRecommend
+//
+//  Created by 多喜和弘 on 2021/04/18.
+//
+
+import UIKit
+
+class MuscleRecordHistoryViewController: UIViewController {
+    // navigationbarのタイトル
+    var navigationBarTitle = ""
+    
+    override func viewDidLoad() {
+        // navigationbarの設定
+        title = navigationBarTitle
+    }
+}
